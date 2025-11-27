edc3ca82d7c0db45c8836f82f2fb0cf3065410f4
jupitters <sampaiobale@gmail.com>
2025-11-26 23:18:48 -0300

    fix start chi

--- cmd/api.go
+++ cmd/api.go
@@ -13,14 +13,16 @@ type application struct {
 
 func (api *application) mount() http.Handler {
 	r := chi.NewRouter()
-	r.Use(middleware.Logger())
-	r.Use(middleware.Recoverer())
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
 
 	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("root."))
 	})
 
 	http.ListenAndServe(":3333", r)
+
+	return r
 }
 
 type config struct {
