0b6380b9c208e4e2dacb89fd7aa6439b23421c53
jupitters <sampaiobale@gmail.com>
2025-11-26 23:12:17 -0300

    mount

--- cmd/api.go
+++ cmd/api.go
@@ -1,7 +1,17 @@
 package main
 
+import (
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+)
+
 type application struct {
 	config config
+}
+
+func (api *application) mount() http.Handler {
+	r := chi.NewRouter()
 }
 
 type config struct {
