56aef402510ad26d490160b5077624cb8e114d8f
jupitters <sampaiobale@gmail.com>
2025-11-27 14:29:50 -0300

    better logs

--- cmd/main.go
+++ cmd/main.go
@@ -15,6 +15,9 @@ func main() {
 		config: cfg,
 	}
 
+	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
+	slog.SetDefault(logger)
+
 	if err := api.run(api.mount()); err != nil {
 		slog.Error("Server failed starting", "error", err)
 		os.Exit(1)
