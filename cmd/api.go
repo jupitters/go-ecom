fefe46a938c6894cf03bd269b95ef2973fa9791e
jupitters <sampaiobale@gmail.com>
2025-11-26 23:16:11 -0300

    start chi

--- cmd/api.go
+++ cmd/api.go
@@ -2,9 +2,6 @@ package main
 
 import (
 	"net/http"
-
-	"github.com/go-chi/chi/v5"
-	"github.com/go-chi/chi/v5/middleware"
 )
 
 type application struct {
@@ -12,15 +9,7 @@ type application struct {
 }
 
 func (api *application) mount() http.Handler {
-	r := chi.NewRouter()
-	r.Use(middleware.Logger())
-	r.Use(middleware.Recoverer())
 
-	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("root."))
-	})
-
-	http.ListenAndServe(":3333", r)
 }
 
 type config struct {
