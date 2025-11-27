c95e60660a29de10978661e89cfdd74fcb18f0ba
jupitters <sampaiobale@gmail.com>
2025-11-26 23:21:42 -0300

    chi middlewares

--- cmd/api.go
+++ cmd/api.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"net/http"
+	"time"
 
 	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 )
 
 type application struct {
@@ -12,6 +14,13 @@ type application struct {
 
 func (api *application) mount() http.Handler {
 	r := chi.NewRouter()
+
+	r.Use(middleware.RequestID)
+	r.Use(middleware.RealIP)
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
+
+	r.Use(middleware.Timeout(60 * time.Second))
 
 	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("root."))
