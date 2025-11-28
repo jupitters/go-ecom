90929177931cd80fe53b5e7a04fba44710cbbc25
jupitters <sampaiobale@gmail.com>
2025-11-27 22:32:05 -0300

    service constructor

--- internal/products/service.go
+++ internal/products/service.go
@@ -5,3 +5,14 @@ import "context"
 type Service interface {
 	ListProducts(ctx context.Context) error
 }
+
+type svc struct {
+}
+
+func NewService() Service {
+	return &svc{}
+}
+
+func (s *svc) ListProducts(ctx context.Context) error {
+	return nil
+}
