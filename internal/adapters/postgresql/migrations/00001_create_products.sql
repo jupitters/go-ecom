1277b862e602b69a4d8f68d95271762a81a1ba94
jupitters <sampaiobale@gmail.com>
2025-11-28 13:34:44 -0300

    create table

--- internal/adapters/postgresql/migrations/00001_create_products.sql
+++ internal/adapters/postgresql/migrations/00001_create_products.sql
@@ -1,9 +1,15 @@
 -- +goose Up
 -- +goose StatementBegin
-SELECT 'up SQL query';
+CREATE TABLE IF NOT EXISTS products (
+    id BIGSERIAL PRIMARY KEY,
+    name TEXT NOT NULL,
+    price_in_centers INTEGER NOT NULL CHECK (price_in_centers >= 0),
+    quantity INTEGER NOT NULL DEFAULT 0,
+    created_at TIMESTAMPTZ NOT NULL DEFAULT now()
+);
 -- +goose StatementEnd
 
 -- +goose Down
 -- +goose StatementBegin
-SELECT 'down SQL query';
+
 -- +goose StatementEnd
