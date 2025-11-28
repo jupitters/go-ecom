bcb6f0aab370060e07dffe83a8d29c5713bb8249
jupitters <sampaiobale@gmail.com>
2025-11-28 14:06:22 -0300

    connect to db

--- cmd/main.go
+++ cmd/main.go
@@ -3,13 +3,23 @@ package main
 import (
 	"log/slog"
 	"os"
+
+	"github.com/jackc/pgx/v5"
 )
 
 func main() {
 	cfg := config{
 		addr: ":8080",
-		db:   dbConfig{},
+		db: dbConfig{
+			dsn: "host=localhost user=postgres password=postgres dbname=ecom sslmode=disabled",
+		},
 	}
+
+	conn, err := pgx.Connect(ctx, cfg.db.dsn)
+	if err != nil {
+		return err
+	}
+	defer conn.Close(ctx)
 
 	api := application{
 		config: cfg,
