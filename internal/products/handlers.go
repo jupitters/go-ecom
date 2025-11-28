eaf304b7afd416234d3530cb0201a5a98d8ca024
jupitters <sampaiobale@gmail.com>
2025-11-27 22:20:56 -0300

    list products handler

--- internal/products/handlers.go
+++ internal/products/handlers.go
@@ -1,10 +1,5 @@
 package products
 
-import (
-	"encoding/json"
-	"net/http"
-)
-
 type handler struct {
 	service Service
 }
@@ -13,13 +8,4 @@ func NewHandler(service Service) *handler {
 	return &handler{
 		service: service,
 	}
-}
-
-func (h *handler) ListProducts(w http.ResponseWriter, r *http.Request) {
-	// var products []string
-	products := []string{}
-
-	w.WriteHeader(http.StatusOK)
-	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(products)
 }
