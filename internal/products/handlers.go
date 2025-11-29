77d369084c812e2faf60961e9f74fc6060456863
jupitters <sampaiobale@gmail.com>
2025-11-29 15:25:07 -0300

    handlers

--- internal/products/handlers.go
+++ internal/products/handlers.go
@@ -1,5 +1,13 @@
 package products
 
+import (
+	"net/http"
+
+	"log"
+
+	myjson "github.com/jupitters/go-ecom/internal/json"
+)
+
 type handler struct {
 	service Service
 }
@@ -8,4 +16,19 @@ func NewHandler(service Service) *handler {
 	return &handler{
 		service: service,
 	}
+}
+
+func (h *handler) ListProducts(w http.ResponseWriter, r *http.Request) {
+	products, err := h.service.ListProducts(r.Context())
+	if err != nil {
+		log.Println(err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	myjson.Write(w, http.StatusOK, products)
+}
+
+func (h *handler) GetProductById(w http.ResponseWriter, r *http.Request) {
+
 }
