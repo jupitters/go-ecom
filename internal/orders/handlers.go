8e37ebc25e9f60017cf8f3db3118e0fa3bf51e6b
jupitters <sampaiobale@gmail.com>
2025-11-29 15:33:14 -0300

    place order

--- internal/orders/handlers.go
+++ internal/orders/handlers.go
@@ -1,5 +1,11 @@
 package orders
 
+import (
+	"net/http"
+
+	myjson "github.com/jupitters/go-ecom/internal/json"
+)
+
 type handler struct {
 	service Service
 }
@@ -8,4 +14,8 @@ func NewHandler(service Service) *handler {
 	return &handler{
 		service: service,
 	}
+}
+
+func (h *handler) PlaceOrder(w http.ResponseWriter, r *http.Request) {
+	myjson.Write(w, http.StatusCreated, nil)
 }
