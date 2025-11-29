0122f71d55841b5f713bf4c71063cb1339ffbea7
jupitters <sampaiobale@gmail.com>
2025-11-29 15:36:00 -0300

    place order orders

--- internal/orders/service.go
+++ internal/orders/service.go
@@ -1,1 +1,21 @@
 package orders
+
+import (
+	"context"
+
+	repo "github.com/jupitters/go-ecom/internal/adapters/postgresql/sqlc"
+)
+
+type svc struct {
+	repo *repo.Queries
+}
+
+func NewService(repo *repo.Queries) Service {
+	return &svc{
+		repo: repo,
+	}
+}
+
+func (s *svc) PlaceOrder(ctx context.Context, tempOrder createOrderParams) (repo.Order, error) {
+
+}
