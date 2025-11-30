225461593ee30fef8902bf1e5c017555530cb3b9
jupitters <sampaiobale@gmail.com>
2025-11-30 17:25:23 -0300

    json write

--- internal/json/json.go
+++ internal/json/json.go
@@ -5,12 +5,6 @@ import (
 	"net/http"
 )
 
-func Write(w http.ResponseWriter, status int, data any) {
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(status)
-	json.NewEncoder(w).Encode(data)
-}
-
 func Read(r *http.Request, data any) error {
 	decoder := json.NewDecoder(r.Body)
 	decoder.DisallowUnknownFields()
