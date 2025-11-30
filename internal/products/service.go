8a96156773aa92e16965af9af14ffe3d802cfcb1
jupitters <sampaiobale@gmail.com>
2025-11-30 17:25:10 -0300

    service products

--- internal/products/service.go
+++ internal/products/service.go
@@ -13,3 +13,12 @@ type Service interface {
 type svc struct {
 	repo repo.Querier
 }
+
+func NewService(repo repo.Querier) Service {
+	return &svc{repo: repo}
+}
+
+func (s *svc) ListProducts(ctx context.Context) ([]repo.Product, error) {
+	return s.repo.ListProducts(ctx)
+
+}
