0a415be0f4ed6d06f421f91f5446bf5b9e44a0cc
jupitters <sampaiobale@gmail.com>
2025-11-30 17:26:53 -0300

    main

--- cmd/main.go
+++ cmd/main.go
@@ -5,6 +5,7 @@ import (
 	"log/slog"
 	"os"
 
+	"github.com/jackc/pgx/v5"
 	"github.com/jupitters/go-ecom/internal/env"
 )
 
@@ -16,6 +17,17 @@ func main() {
 		db: dbConfig{
 			dsn: env.GetString("GOOSE_DBSTRING", "host=localhost user=postgres password=postgres dbname=ecom sslmode=disable"),
 		},
+	}
+
+	conn, err := pgx.Connect(ctx, cfg.db.dsn)
+	if err != nil {
+		panic(err)
+	}
+	defer conn.Close(ctx)
+
+	api := application{
+		config: cfg,
+		db:     conn,
 	}
 
 	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
