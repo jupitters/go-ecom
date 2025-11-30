b83c8159613c08824774e80fcf3ce371ed99f64c
jupitters <sampaiobale@gmail.com>
2025-11-30 17:24:29 -0300

    orders types

--- internal/orders/types.go
+++ internal/orders/types.go
@@ -1,1 +1,21 @@
 package orders
+
+import (
+	"context"
+
+	repo "github.com/jupitters/go-ecom/internal/adapters/postgresql/sqlc"
+)
+
+type OrderItem struct {
+	ProductID int64 `json:"product_id"`
+	Quantity  int32 `json:"quantity"`
+}
+
+type createOrderParams struct {
+	CustomerID int64       `json:"customer_id"`
+	Items      []OrderItem `json:"items"`
+}
+
+type Service interface {
+	PlaceOrder(ctx context.Context, tempOrder createOrderParams) (repo.Order, error)
+}
