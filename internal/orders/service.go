02590f00c6506b0df3ebaa8aae368f27e6c17ed3
jupitters <sampaiobale@gmail.com>
2025-11-30 17:23:03 -0300

    handler orders

--- internal/orders/service.go
+++ internal/orders/service.go
@@ -1,9 +1,6 @@
 package orders
 
 import (
-	"context"
-	"fmt"
-
 	repo "github.com/jupitters/go-ecom/internal/adapters/postgresql/sqlc"
 )
 
@@ -15,14 +12,4 @@ func NewService(repo *repo.Queries) Service {
 	return &svc{
 		repo: repo,
 	}
-}
-
-func (s *svc) PlaceOrder(ctx context.Context, tempOrder createOrderParams) (repo.Order, error) {
-	if tempOrder.CustomerID == 0 {
-		return repo.Order{}, fmt.Errorf("CustomerID is required!")
-	}
-	if len(tempOrder.Items) == 0 {
-		return repo.Order{}, fmt.Errorf("At least one item is required!")
-	}
-
 }
